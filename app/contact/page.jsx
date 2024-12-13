0c56bf8d4ec478282eb7aa0ca397e69cecb531c7
Than Zaw Htet <tzhtet@mit.com.mm>
2024-12-13 22:23:42 +0630

    just-portfolio

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,10 @@
 "use client";
 
-import {Button} from "@/components/ui/button";
-import {Input} from "@/components/ui/input";
-import {Textarea} from "@/components/ui/textarea";
-
+import { useRef, useState } from "react";
+import emailjs from "emailjs-com";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import {
     Select,
     SelectContent,
@@ -11,10 +12,10 @@ import {
     SelectItem,
     SelectLabel,
     SelectTrigger,
-    SelectValue
+    SelectValue,
 } from "@/components/ui/select";
-
 import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const info = [
     {
@@ -31,56 +32,105 @@ const info = [
         icon: <FaMapMarkedAlt />,
         title: "Address",
         description: "MIT Innovation Hub\n 656, E Gyo Gone St, Yangon",
-    }
+    },
 ];
 
-import {motion} from "framer-motion";
-
 const Contact = () => {
+    const formRef = useRef(null);
+    const [message, setMessage] = useState("");
+    const [isSuccess, setIsSuccess] = useState(false); // Track submission success
+    const [selectedService, setSelectedService] = useState(""); // Add this line to manage dropdown state
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "service_zfm8cugtzhtet",
+                "template_118rh6p",
+                formRef.current,
+                "wU4wqnM2lySzbIQzn"
+            )
+            .then((result) => {
+                setIsSuccess(true);
+                setMessage("Email sent successfully!");
+            })
+            .catch((error) => {
+                setIsSuccess(false);
+                setMessage("Failed to send email: " + error.text);
+            });
+    };
+
     return (
         <motion.section
-            initial={{opacity: 0}}
-            animate={{opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' },}}
+            initial={{ opacity: 0 }}
+            animate={{
+                opacity: 1,
+                transition: { delay: 0.4, duration: 0.4, ease: "easeIn" },
+            }}
             className="py-6"
         >
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row gap-[30px]">
-                    {/* form */}
-                    <div className="xl:h-[54%] order-2 xl:order-none">
-                        <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-                            <h3 className="text-4xl text-accent">Let&#39;s work together</h3>
-                            <p className="text-white/60">
-                                I’m passionate about building efficient, user-friendly applications.
-                                Whether you need a
-                                reliable developer for your team or a freelancer for your project,
-                                I’d love to collaborate and bring your ideas to life.
-                            </p>
-                            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                                <Input type="firstname" placeholder="First Name" />
-                                <Input type="lastname" placeholder="Last Name" />
-                                <Input type="email" placeholder="Email address" />
-                                <Input type="phone" placeholder="Phone number" />
-
-                            </div>
-                            {/* Select */}
-                            <Select>
-                                <SelectTrigger>
-                                    <SelectValue>
-
-                                    </SelectValue>
+                    {/* Form */}
+                    <div className="xl:w-[54%] order-2 xl:order-none">
+                        <form
+                            ref={formRef}
+                            onSubmit={sendEmail}
+                            className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+                        >
+                            <Input type="text" name="name" placeholder="Full Name"/>
+                            <Input type="email" name="email" placeholder="Email address"/>
+                            <Input type="text" name="phone" placeholder="Phone number"/>
+                            <Input type="hidden" name="selectedService" value={selectedService}/>
+                            <Select onValueChange={(value) => setSelectedService(value)}>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Select a service"/>
                                 </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectItem value="Web Development">Web Development</SelectItem>
+                                        <SelectItem value="Spring Backend">Spring Backend</SelectItem>
+                                        <SelectItem value="Cloud Solutions">Cloud Solutions</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
                             </Select>
+                            <Textarea
+                                name="message"
+                                className="h-[200px]"
+                                placeholder="Type your message here."
+                            />
+                            <Button type="submit" size="md" className="max-w-40">
+                                Send message
+                            </Button>
                         </form>
                     </div>
-                    {/* info */}
-                    <div className="flex-1 items-center xl:justify-end order-1
-                    xl:order-none mb-8 xl:mb-0">
-                        info
+                    {/* Info */}
+                    <div
+                        className="flex-1 items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0"
+                    >
+                        <ul className="flex flex-col gap-10">
+                            {info.map((item, index) => (
+                                <li key={index} className="flex items-center gap-6">
+                                    <div
+                                        className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px]
+                                            bg-[#27272c] text-accent rounded-md flex items-center
+                                            justify-center"
+                                    >
+                                        <div className="text-[28px]">{item.icon}</div>
+                                    </div>
+                                    <div className="flex-1">
+                                        <p className="text-white/60">{item.title}</p>
+                                        <h3 className="text-xl">{item.description}</h3>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>
         </motion.section>
-    )
-}
+    );
+};
 
-export  default  Contact;
+export default Contact;
--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import Social from "@/components/Social";
 import Photo from "@/components/Photo";
 import Stats from "@/components/Stats";
 
+import Link from 'next/link';
 
 const Home = () => {
   return (
@@ -23,19 +24,22 @@ const Home = () => {
               </p>
               {/* btn and socials */}
               <div className="flex flex-col xl:flex-row items-center gap-8">
-                <Button
-                    variant="outline"
-                    size="lg"
-                    className="uppercase flex items-center gap-2">
-                  <span>Download CV </span>
+                <a
+                    href="/assets/cv/tzhtet_cv.pdf"
+                    download="Than_Zaw_Htet_CV.pdf"
+                    className="flex items-center gap-2 px-6 py-3 border
+                     border-accent text-accent rounded-full
+                     hover:bg-accent hover:text-primary transition-all duration-500"
+                >
+                  <span>Download CV</span>
                   <FiDownload className="text-xl"/>
-                </Button>
-                <div className="mb-8 xl:mb-0">
-                  <Social containerStyles="flex gap-6" iconsStyles="w-9 h-9 border
+                </a>
+                  <div className="mb-8 xl:mb-0">
+                    <Social containerStyles="flex gap-6" iconsStyles="w-9 h-9 border
                   border-accent rounded-full flex justify-center items-center
                   text-accent text-base hover:bg-accent hover:text-primary
                   hover:transition-all duration-500"/>
-                </div>
+                  </div>
               </div>
             </div>
             {/* photo */}
@@ -46,7 +50,7 @@ const Home = () => {
         </div>
         <Stats/>
       </section>
-  );
+);
 };
 
 export default Home;
